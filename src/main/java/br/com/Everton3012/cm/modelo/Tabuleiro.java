f2b33944a4a3772afb7df05e3cb92ca670869d95
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2026-08-20 00:03:21 -0300

    Adiciona a classe Tabuleiro com atributos e métodos para gerar campos, associar vizinhos e sortear minas

--- src/main/java/br/com/Everton3012/cm/modelo/Tabuleiro.java
+++ src/main/java/br/com/Everton3012/cm/modelo/Tabuleiro.java
@@ -1,5 +1,39 @@
 package main.java.br.com.Everton3012.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
 
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    private final List<Campo> campos = new ArrayList<>();
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarVizinhos();
+        sortearMinas();
+    }
+
+    private void gerarCampos() {
+        for (int linha = 0; linha < linhas; linha++) {
+            for (int coluna = 0; coluna < colunas; coluna++) {
+                campos.add(new Campo(linha, coluna));
+            }
+        }
+    }
+
+    private void associarVizinhos() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void sortearMinas() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
